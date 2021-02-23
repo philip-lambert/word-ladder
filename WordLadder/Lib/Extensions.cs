67dbd5b9b62545af7efc7549ee1e828b088ca618
Philip Lambert <plambert@westwoodenergy.com>
2021-02-23 17:19:16 +0000

    Added basic string handling

---
+++ WordLadder/Lib/Extensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace WordLadder.Lib
+{
+  internal static class Extensions
+  {
+    internal static bool IsValid(this string str)
+    {
+      if (str == null)
+        return false;
+
+      str = str.Trim();
+
+      if (str.Length != Constants.MAX_WORD_LENGTH)
+        return false;
+
+      if (str.Except(Constants.VALID_WORD_CHARS).Any())
+        return false;
+
+      return true;
+    }
+
+    internal static bool IsOneLetterDifferent(this string str1, string str2, bool caseSensitive = false)
+    {
+      if (!str1.IsValid() || !str2.IsValid())
+        return false;
+
+      if (!caseSensitive)
+      {
+        str1 = str1.ToUpper();
+        str2 = str2.ToUpper();
+      }
+
+      int difference = 0;
+      for (int loop = 0; loop < str1.Length; loop++)
+        if (str1[loop] != str2[loop])
+          difference++;
+      bool result = difference == 1;
+      return result;
+    }
+  }
+}
---
+++ WordLadder/Tests/ExtensionsTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using WordLadder.Lib;
+
+namespace WordLadder.Tests
+{
+  [TestClass]
+  public class ExtensionsTests
+  {
+    [DataTestMethod]
+    // IsValid should return false
+    [DataRow(null, false)]
+    [DataRow("", false)]
+    [DataRow("c", false)]
+    [DataRow("cas", false)]
+    [DataRow("cases", false)]
+    [DataRow("c@se", false)]
+    // IsValid should return true
+    [DataRow("case", true)]
+    [DataRow("Case", true)]
+    public void IsValid_ReturnsExpected_ForInputs(string str, bool expected)
+    {
+      bool actual = str.IsValid();
+      Assert.AreEqual(expected, actual);
+    }
+
+    [DataTestMethod]
+    // IsOneLetterDifferent should return false
+    [DataRow(null, "cast", false, false)]
+    [DataRow("", "cast", false, false)]
+    [DataRow("c", "cast", false, false)]
+    [DataRow("cas", "cast", false, false)]
+    [DataRow("cases", "cast", false, false)]
+    [DataRow("c@se", "cast", false, false)]
+    [DataRow("case", null, false, false)]
+    [DataRow("case", "", false, false)]
+    [DataRow("case", "", false, false)]
+    [DataRow("case", "cas", false, false)]
+    [DataRow("case", "castle", false, false)]
+    [DataRow("case", "c@st", false, false)]
+    [DataRow("case", "CAST", true, false)]
+    [DataRow("CASE", "cast", true, false)]
+    [DataRow("case", "vast", false, false)]
+    [DataRow("same", "cost", false, false)]
+    [DataRow("tsac", "cast", false, false)]
+    [DataRow("case", "cats", false, false)]
+    // IsOneLetterDifferent should return true
+    [DataRow("case", "cast", false, true)]
+    [DataRow("CASE", "cast", false, true)]
+    [DataRow("cast", "case", false, true)]
+    [DataRow("cast", "CASE", false, true)]
+    public void IsOneLetterDifferent_ReturnsExpected_ForInputs(string str1, string str2, bool caseSensitive, bool expected)
+    {
+      bool actual = str1.IsOneLetterDifferent(str2, caseSensitive);
+      Assert.AreEqual(expected, actual);
+    }
+  }
+}
