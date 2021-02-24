f8237cd344518e2f3f30de4b394934acb0483114
Philip Lambert <plambert@westwoodenergy.com>
2021-02-24 01:02:21 +0000

    DictionaryParser

---
+++ WordLadder/Lib/DictionaryParser/DictionaryParser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using WordLadder.Lib.DictionaryLoader;
+
+namespace WordLadder.Lib.DictionaryParser
+{
+  public class DictionaryParser
+  {
+    private string _start;
+    private string _end;
+    private string[] _dictionary;
+
+    public DictionaryParser(string start, string end, IDictionaryLoader loader)
+    {
+      if (string.IsNullOrWhiteSpace(start))
+        throw new ArgumentNullException(nameof(start));
+      if (string.IsNullOrWhiteSpace(end))
+        throw new ArgumentNullException(nameof(end));
+      if (loader == null)
+        throw new ArgumentNullException(nameof(loader));
+
+      _start = start;
+      _end = end;
+      _dictionary = loader.Load();
+    }
+
+    public string[] Parse()
+    {
+      return Array.Empty<string>();
+    }
+  }
+}
--- WordLadder/Program.cs
+++ WordLadder/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 
 using WordLadder.Lib.DictionaryLoader;
+using WordLadder.Lib.DictionaryParser;
 
 namespace WordLadder
 {
@@ -21,23 +23,32 @@ namespace WordLadder
         return;
       }
 
-      IDictionaryLoader dictionaryLoader = DictionaryLoaderFactory.Create(inputArgs.Dictionary);
-
-      Console.WriteLine("Get on with it!");
-      Console.ReadLine();
+      try
+      {
+        IDictionaryLoader loader = DictionaryLoaderFactory.Create(inputArgs.Dictionary);
+        DictionaryParser parser = new DictionaryParser(inputArgs.Start, inputArgs.End, loader);
+        string[] result = parser.Parse();
+        File.WriteAllLines(inputArgs.Output, result);
+      }
+      catch (Exception ex)
+      {
+        WriteLine();
+        WriteLine("An unknown error occurred: -");
+        WriteLine(ex.Message, true);
+      }
     }
 
     private static void ShowHelp(InputArgs inputArgs)
     {
-      WriteLine("Computes a list of words which move from the start word to the end word in the shortest number of steps.", false);
-      WriteLine(string.Empty, false);
+      WriteLine("Computes a list of words which move from the start word to the end word in the shortest number of steps.");
+      WriteLine();
       WriteLine($"  {InputArgs.START_ARG}\t\tFour letter start word i.e. 'same'", inputArgs != null && !inputArgs.StartIsValid);
       WriteLine($"  {InputArgs.END_ARG}\t\t\tFour letter end word i.e. 'cost'", inputArgs != null && !inputArgs.EndIsValid);
       WriteLine($"  {InputArgs.DICTIONARY_ARG}\t\tDictionary file name", inputArgs != null && !inputArgs.DictionaryIsValid);
       WriteLine($"  {InputArgs.OUTPUT_ARG}\t\tAnswer file name", inputArgs != null && !inputArgs.OutputIsValid);
     }
 
-    private static void WriteLine(string str, bool showAsError)
+    private static void WriteLine(string str = "", bool showAsError = false)
     {
       if (!showAsError)
         Console.WriteLine(str);
